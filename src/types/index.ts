2388cf5213d254254ffdc21b15d44a2e1574e4ec
Ali Jafari <alisedghjafari@gmail.com>
2024-07-20 06:39:27 -0700

    gggg

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,10 @@
 // ====== USER PARAMS
 export type CreateUserParams = {
   clerkId: string;
-  // firstName: string;
-  // lastName: string;
+  firstName: string;
+  lastName: string;
   username: string;
-  // email: string;
+  email: string;
   photo: string;
 };
 
